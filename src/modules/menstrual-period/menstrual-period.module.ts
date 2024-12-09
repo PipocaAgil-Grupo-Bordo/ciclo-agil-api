7c7e36ec1183d1e8eb8c337aa4ce72fcd50f7193
Rayandson <rayandson_silva@hotmail.com>
2024-12-09 16:43:28 -0300

    add menstrual period controller v2

--- src/modules/menstrual-period/menstrual-period.module.ts
+++ src/modules/menstrual-period/menstrual-period.module.ts
@@ -2,28 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailModule } from '../../shared/services/email/email.module';
 import { EncryptionModule } from '../../shared/services/encryption/encryption.module';
-import { ProfileModule } from '../profile/profile.module';
-import { ProfileRepository } from '../profile/profile.repository';
 import { MenstrualPeriod } from './entities/menstrual-period.entity';
 import { MenstrualPeriodDateRepository } from './menstrual-period-date.repository';
 import { MenstrualPeriodController } from './menstrual-period.controller';
 import { MenstrualPeriodRepository } from './menstrual-period.repository';
 import { MenstrualPeriodService } from './menstrual-period.service';
+import { MenstrualPeriodV2Controller } from './menstrual-period-v2.controller';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([MenstrualPeriod]),
-        EncryptionModule,
-        EmailModule,
-        ProfileModule,
-    ],
+    imports: [TypeOrmModule.forFeature([MenstrualPeriod]), EncryptionModule, EmailModule],
     exports: [MenstrualPeriodService],
-    controllers: [MenstrualPeriodController],
-    providers: [
-        MenstrualPeriodService,
-        MenstrualPeriodRepository,
-        MenstrualPeriodDateRepository,
-        ProfileRepository,
-    ],
+    controllers: [MenstrualPeriodController, MenstrualPeriodV2Controller],
+    providers: [MenstrualPeriodService, MenstrualPeriodRepository, MenstrualPeriodDateRepository],
 })
 export class MenstrualPeriodModule {}
