e27920e405f9965acf029bd8f3f907b163913007
isakoga <isadorakoga710@gmail.com>
2025-06-23 19:22:02 -0300

    feat: create user decorator for whoami endpoint

---
+++ src/shared/types/auth-payload.interface.ts
@@ -0,0 +1,9 @@
+export interface AuthPayload {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    email: string;
+    password: string;
+    birthdate: string;
+}
